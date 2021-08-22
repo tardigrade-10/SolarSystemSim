9b2461169afcefe4607b0fdca27a2ece19334be6
YashYadav <50978045+tardigrade-10@users.noreply.github.com>
2021-08-23 00:11:04 +0530

    Update sim.py

--- sim.py
+++ sim.py
@@ -6,8 +6,8 @@ import matplotlib.animation as animation
 from astropy.time import Time
 from astroquery.jplhorizons import Horizons
 
-sim_start_date = "2021-08-21" 
-sim_duration = 5*365
+start_date = "2021-08-21" 
+duration = 5*365
 
 class Object:
     def __init__(self, name, rad, color, r, v):
@@ -47,32 +47,34 @@ class SolarSystem:
         return plots+lines+[self.timestamp]
 
 img = plt.imread('bg2.jpg')
-# plt.style.use('dark_background')
 fig = plt.figure(figsize=[6,6])
-
-ax=plt.axes([0.,0.,1.,1.], xlim=(-4.0, 4.0), ylim=(-2.0, 2.0))
+ax = plt.axes([0.,0.,1.,1.], xlim=(-4.0, 4.0), ylim=(-2.0, 2.0))
 ax.set_aspect('equal')
 ax.axis('off')
 ax.imshow(img, extent = [-4, 4, -2, 2])
-ss = SolarSystem(Object("Sun", 28, 'red', [0,0,0],[0,0,0]))
-ss.time = Time(sim_start_date).jd
-colors = ['grey','orange', 'blue', 'chocolate']
-sizes = [0.38, 0.95, 1.0, 0.53]
+
+colors = ['red','cyan', 'blue', 'chocolate']
+sizes = [0.38, 0.95, 1.0, 0.53]                                    #planets actual relative sizes (radius)
 names = ['Mercury', 'Venus', 'Earth', 'Mars']
 textdfc = [0.47, 0.73, 1, 1.5]                 #text distance from center
 
+ss = SolarSystem(Object("Sun", 30, 'orange', [0,0,0],[0,0,0]))
+ss.time = Time(start_date).jd
+
 for i, nasaid in enumerate([199,299,399,499]):
     obj = Horizons(id=nasaid, location="@sun", epochs=ss.time, id_type='id').vectors()
-    ss.add_planet(Object(nasaid, 20*sizes[i], colors[i], [np.double(obj[xi]) for xi in ['x','y','z']], [np.double(obj[vxi]) for vxi in ['vx','vy','vz']]))
+    ss.add_planet(Object(nasaid, 15*sizes[i], colors[i], [np.double(obj[xi]) for xi in ['x','y','z']], [np.double(obj[vxi]) for vxi in ['vx','vy','vz']]))
     ax.text(0, -(textdfc[i]+0.1), names[i], color=colors[i], zorder=1000, ha='center', fontsize='small')
 
 def animate(i):
     return ss.evolve()
 
-ani = animation.FuncAnimation(fig, animate, repeat = False, frames = sim_duration, blit = True, interval = 20)
+ani = animation.FuncAnimation(fig, animate, repeat = False, frames = duration, blit = True, interval = 20)
 
+f = "animation.gif" 
+writergif = animation.PillowWriter(fps=30) 
+ani.save(f, writer=writergif)
 plt.show()
 
-# ani.save('solar_system_6in_150dpi.mp4', fps=60, dpi=150)
 
 # python sim.py
